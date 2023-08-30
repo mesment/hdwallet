96919c33154c12ea824af88fa5bc40f921aa9900
mesment <mesment7826@gmail.com>
2023-08-30 11:55:55 +0800

    Update transact.go

--- eth/transact.go
+++ eth/transact.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/lizc2003/hdwallet/wallet"
+	"github.com/mesment/hdwallet/wallet"
 	"math/big"
 )
 
